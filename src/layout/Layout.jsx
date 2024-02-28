0a34314eae72e653d11995fe96a8f5d8120a0dd7
mara <marmysticscribe@gmail.com>
2024-02-28 20:58:02 +0600

    project structure

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import styles from "./Layout.module.css"
+import App from "../pages/app/App"
+
+function Layout() {
+  return (
+		<div className="Layout">
+			<App/>
+		</div>
+	)
+}
+
+export default Layout
