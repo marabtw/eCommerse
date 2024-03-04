b82f28de301a3c6f2842e610c5be4a44786019d5
mara <marmysticscribe@gmail.com>
2024-03-04 20:49:42 +0500

    fix

--- src/components/slider/mainSlider/mainSlider.jsx
+++ src/components/slider/mainSlider/mainSlider.jsx
@@ -18,9 +18,15 @@ const MainSlider = () => {
                   "Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis. "
                 }
               />
+              <Description
+                text={
+                  "Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis. "
+                }
+              />
               <Button text="contact us" color="red" uppercase={true} />
             </div>
           </div>
+
           <div className={styles.slide__column}>
             <img src={sliderImg} />
           </div>
