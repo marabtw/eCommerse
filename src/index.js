492f4dc066534fb7aa4bc43e2a76770431f21855
mara <marmysticscribe@gmail.com>
2024-03-12 23:04:35 +0500

    Add Redux store with basic reducer functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import "./assets/styles/globals.scss"
 import Layout from "./layout/Layout"
 import reportWebVitals from "./utils/reportWebVitals"
 import { BrowserRouter as Router } from "react-router-dom"
+import { createStore } from "redux"
+
+const defaultState = {
+	user: null,
+}
+
+const reducer = (state, action) => {
+	switch(action.type) {
+		case "auth":
+			return {...state, user: action.payload}
+		default:
+			return state
+	}
+}
+
+const store = createStore()
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
